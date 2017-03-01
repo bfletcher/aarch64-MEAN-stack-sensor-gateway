f84989315f525c5de05541b10fa477f9d48f6633
Bill Fletcher <bill.fletcher@linaro.org>
2017-03-01 15:40:59 +0000

    renamed charts api to ui

--- mean_sensor_apis/server_start.sh
+++ mean_sensor_apis/server_start.sh
@@ -4,9 +4,9 @@ if [ "$1" = 'sensor-api' ]; then
    node sensor-api.js
 elif [ "$1" = 'admin-api' ]; then
    node admin-api.js
-elif [ "$1" = 'charts-api' ]; then
-   node charts-api.js
+elif [ "$1" = 'ui-api' ]; then
+   node ui-api.js
 else
-   echo "usage: choose a server (admin-api, sensor-api) to start"
+   echo "usage: choose a server (admin-api, sensor-api, ui-api) to start"
 fi
 
